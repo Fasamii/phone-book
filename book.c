2017a57d3c7ac491fe1634800cb4ed1ff7feec85
fasamii <fasami0512@gmail.com>
2024-10-08 15:46:42 +0200

    improved memory safety

--- book.c
+++ book.c
@@ -26,17 +26,17 @@ void print_linkedlist(linked_list *node) {
 
 linked_list *get_user_input() {
     linked_list *node = malloc(sizeof(linked_list));
+    if(node == NULL) { free(node); return NULL; }
     linked_list *parent = node;
     int count = 0;
     char ac_char;
     while((ac_char = getchar()) != '\n') {
         if(ac_char != ' ') {
             node->data = realloc(node->data, (count + 1) * sizeof(char));
-            node->data[count] = ac_char;
-            count++;
+            node->data[count++] = ac_char;
         } else if(count > 0) {
-            node->next = malloc(sizeof(linked_list));
-            node = node->next;
+            node = node->next = malloc(sizeof(linked_list));
+            if(node == NULL) { free_linkedlist(parent); return NULL; }
             node->next = NULL;
             node->data = NULL;
             count = 0;
@@ -48,6 +48,10 @@ linked_list *get_user_input() {
 int main(void) {
 
     linked_list *node = get_user_input();
-    print_linkedlist(node);
-    free_linkedlist(node);
+    if(node != NULL) {
+        print_linkedlist(node);
+        free_linkedlist(node);
+        return 0;
+    }
+    return -1;
 }
